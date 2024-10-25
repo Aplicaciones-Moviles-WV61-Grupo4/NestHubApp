db14edeae37b999a0962692a85f8ce4baa21c1fe
Angel Cancho <u201721995@upc.edu.pe>
2024-10-25 15:53:57 -0500

    feat: added price setting screen

---
+++ lib/price_setting_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Importar FilteringTextInputFormatter
+
+class PriceSettingScreen extends StatelessWidget {
+  const PriceSettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Establece el precio',
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Puedes cambiarlo cuando quieras',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 61, 61, 61),
+                ),
+              ),
+              const SizedBox(height: 32),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Ingresa el precio',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+              ),
+              const SizedBox(height: 32),
+              const Text(
+                'Precio para el huésped (no incluye los impuestos), con impuestos sería S/. 120',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 61, 61, 61),
+                ),
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: const Text('Atrás'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 1, 134, 72),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text('Siguiente'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
