9ba4f1ac333338ac50b04e7a140383733ff2d281
Angel Cancho <u201721995@upc.edu.pe>
2024-10-26 01:50:55 -0500

    feat(login): center content in LoginScreen to improve layout alignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nesthub/login_screen.dart';
 import 'package:nesthub/step_1_page.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Step1Page(),
+      home: LoginScreen(),
     );
   }
 }
