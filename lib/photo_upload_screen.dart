23103d98ebb21fd7409e28bae10d6882e39d9809
BGamio <u202120344@upc.edu.pe>
2024-10-26 19:59:14 -0500

    feat: add open camera and gallery

--- lib/photo_upload_screen.dart
+++ lib/photo_upload_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:nesthub/step_2_page.dart';
-import 'package:nesthub/step_3_page.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'dart:io';
 import 'package:nesthub/title_description_screen.dart';
 
-class PhotoUploadScreen extends StatelessWidget {
+class PhotoUploadScreen extends StatefulWidget {
   const PhotoUploadScreen({super.key});
+
+  @override
+  State<PhotoUploadScreen> createState() => _PhotoUploadScreenState();
+}
+
+class _PhotoUploadScreenState extends State<PhotoUploadScreen> {
+  final ImagePicker _picker = ImagePicker();
+  List<File> _images = [];
+
+  Future<void> _pickImage(ImageSource source) async {
+    if (source == ImageSource.camera) {
+      await _openGallery();
+    } else {
+      final XFile? pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          _images.add(File(pickedFile.path));
+        });
+      }
+    }
+  }
+
+    Future<void> _openGallery() async {
+    final permitted = await PhotoManager.requestPermissionExtend();
+    if (!permitted.isAuth) return;
+
+    final List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(onlyAll: true);
+    if (albums.isEmpty) return;
+
+    final List<AssetEntity> assets = await albums[0].getAssetListPaged(page: 0, size: 80);
+    final List<File?> files = await Future.wait(assets.map((asset) => asset.file));
+
+    setState(() {
+      _images.addAll(files.whereType<File>());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +75,7 @@ class PhotoUploadScreen extends StatelessWidget {
                   context,
                   imagePath: 'assets/photo_upload_icons/agregar_fotos.png',
                   label: 'Agrega fotos',
-                  onPressed: () {},
+                  onPressed: () { _pickImage(ImageSource.gallery); },
                 ),
               ),
               const SizedBox(height: 8),
@@ -48,10 +85,23 @@ class PhotoUploadScreen extends StatelessWidget {
                   context,
                   imagePath: 'assets/photo_upload_icons/foto_nueva.png',
                   label: 'Toma fotos nuevas',
-                  onPressed: () {},
+                  onPressed: () { _pickImage(ImageSource.camera); },
                 ),
               ),
               const Spacer(),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                  ),
+                  itemCount: _images.length,
+                  itemBuilder: (context, index) {
+                    return Image.file(_images[index], fit: BoxFit.cover);
+                  },
+                ),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
