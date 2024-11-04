f787a084143c44e61652b8612a9e75d182df9922
MiguelC <sackito992@gmail.com>
2024-11-04 17:07:31 -0500

    feat: add new constant

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -3,4 +3,5 @@ class AppConstants {
       'https://nesthubplatform-0a5eeee622eb.herokuapp.com/api/v1';
   static const String localsEndpoint = '/locals';
   static const String profilesEndpoint = '/profiles';
+  static const String usersEndpoint = '/users';
 }
